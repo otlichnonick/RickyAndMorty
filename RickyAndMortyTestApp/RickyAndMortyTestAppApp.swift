581e02c9b13a5b0024f09d0985b9c5b8083dd6a7
Anton Agafonov <otlichnonick@yandex.ru>
2023-11-14 17:39:51 +0100

    Initial Commit

---
+++ RickyAndMortyTestApp/RickyAndMortyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortyTestAppApp.swift
+//  RickyAndMortyTestApp
+//
+//  Created by Anton Agafonov on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RickyAndMortyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
